03ff563542568d4f2321aac48a598d2038fab960
ishostak <ihor.shostak1@gmail.com>
2021-12-05 23:46:24 +0200

    Test commit Some changes for example

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -3,7 +3,8 @@ package com.alexs;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("hiiii from git");
-        System.out.println("hiiii from git");
+        for (int i = 0; i < 1999; i++) {
+            System.out.println("Few hello's");
+        }
     }
 }

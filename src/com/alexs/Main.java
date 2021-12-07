0fad00e6ed0abd774dd53a50a840bee97898bb1a
ishostak <ihor.shostak1@gmail.com>
2021-12-07 18:01:01 +0200

    Commit for merge

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -3,8 +3,6 @@ package com.alexs;
 public class Main {
 
     public static void main(String[] args) {
-        for (int i = 0; i < 1999; i++) {
-            System.out.println("Few hello's");
-        }
+        System.out.println("Oh, no it's broken");
     }
 }

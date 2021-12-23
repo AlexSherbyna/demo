a966dd2ff913331f089ee67af472add693736f91
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2021-12-23 19:54:07 +0200

    dobavil_demo_2

---
+++ src/com/alexs/demo_2.java
@@ -0,0 +1,12 @@
+public class demo_2 {
+
+    public static void main(String args[]) {
+    double a = 3562,c = 185.5,d ;
+    double b = a/c;
+     d = a /1000;
+        System.out.println(b);
+        System.out.println(d);
+
+    }
+}
+

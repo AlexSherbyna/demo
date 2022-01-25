bde3ed47d6fa5537bffa23db4b31ca415bc59516
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-01-25 16:26:23 +0200

    work with array

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -27,16 +27,55 @@ public class Main {
 
      */
 
-           int mass[] = new int[]{7, 5, 3, 10, 13,};
+           int mass[] = new int[]{10, 4, 3, 9, 11, 8, 15 };
 
-           int a, b, c, v = 1;
+           int a, b = 0, c, v = 1;
 
-           for (a = 0; a < mass.length; a++) {
-               System.out.print(mass[a]);
-               System.out.print(" ");
+
+           for (a = 0; a < mass.length; a++)
+           {
+               System.out.println("********  ");
+               System.out.println(mass[a] + " Chislo massiva");
+               System.out.println(a + " iteracia");
+
+                v = 1;
+
+               for (b = a+1; b <= mass.length; b++) {
+
+
+                   if (mass[a] == (mass[mass.length - 1])) {
+                       System.out.println("Konec");
+                       System.out.println("********  ");
+                       System.out.println("");
+                       break;
+                   }
+
+                   if (mass[a] < mass[b]) {
+                       if (v == 1) {
+                           System.out.println("poteplenie zavtra");
+                           System.out.println("Sravnili s " + mass[b]);
+                           System.out.println("********  ");
+                           System.out.println("");
+                           break;
+                       } else {
+                           System.out.println("Poteplenie  na " + v + " den");
+                           System.out.println("Dney " + v);
+                           System.out.println("Sravnili s  " + mass[b]);
+                           System.out.println("********  ");
+                           System.out.println("");
+                           break;
+                       }
+                   } else {
+
+                       v++;
+                   }
+               }
+
+
+
+
            }
 
 
-
        }
-}
+    }

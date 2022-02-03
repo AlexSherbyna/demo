d460920a09ee86ee1a94f337cad39f0bfc0051a9
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-03 16:18:07 +0200

    If else

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -5,21 +5,10 @@ public class demo_2 {
 
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
-        int a, b, c,v;
+        int a, b, c, v;
         double kor;
-            System.out.println("1 katet");
         a = in.nextInt();
-            System.out.println("2 katet");
-        b = in.nextInt();
-            System.out.println("Gipo");
-        c = in.nextInt();
-         kor = Math.sqrt(a*a + b*b );
-         v = (int) kor;
-
-        System.out.println(v);
-
-        if(c == v ) System.out.println("yes");
-        else System.out.println("No");
+             System.out.println( a*=a) ;
 
     }
 

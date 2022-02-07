20b24ce632c338f34a3b2c6db668ea126ed634fd
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-07 17:42:11 +0200

    Operation_2

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -7,25 +7,7 @@ public class Main {
 
        public static void main(String[] args) {
 
-    /*
-       int a,b,c,sum;
-           System.out.println("Количество крышок_10");
-        a = in.nextInt() * 10;
-           System.out.println("Количество крышок_100");
-        b = in.nextInt() * 100;
-           System.out.println("Количество крышок_1000");
-        c = in.nextInt() * 1000;
-           System.out.println("Количество крышок " + (a+b+c));
 
-
-        int mass [] = {1,3,5,6,11,23},a = 0,sum = 0;
-
-        for(a = 0 ;a < 6;a++ )
-        sum = sum + mass[a];
-
-            System.out.println(sum);
-
-     */
 
            int mass[] = new int[]{10, 4, 3, 9, 11, 8, 15 };
 
--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -5,27 +5,17 @@ public class demo_2 {
 
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
-        int a, b, c = 0, v;
-        double kor;
-        a = in.nextInt();
-        if (a%2!=1)
-        {
-            b=a/2;
-            c=(a+b)%10;
-            System.out.println("+++");
-            System.out.println(b);
-            System.out.println(c);
-            System.out.println(a+b+c);
-        }
-        else
-        {
-            b=a/2+1;
-            c=(a*b)%10;
-            System.out.println("---");
-            System.out.println(b);
-            System.out.println(c);
-            System.out.println( c-a-b);
-        }
+        int  b, c = 0, v;
+        double a;
+        a = in.nextDouble();
+             if(a==(int)a){
+                System.out.println("++");
+
+            } else {
+                System.out.println("Fail_2");
+            }
+
+
     }
 }
 

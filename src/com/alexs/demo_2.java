e575240505d60983268de879854912ad76798c68
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-09 13:29:30 +0200

    Down to

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -6,16 +6,19 @@ public class demo_2 {
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
         int a, b, c ,d, v;
-
+        float a1;
         a = in.nextInt();
-        b =  a/100;      //*--
-        c = (a/10)%10;   //*--
-
-        v =  a%10;       //--*
-       if(b+c == b || c+v == (v|c)) {
-            System.out.println("est 0");
+        if(a%2 == 0){
+            System.out.println(" 2 chislo drob");
+            a1 = in.nextFloat();
+            a1 = (a*a1);
+            System.out.println(a1);
+        } else {
+            System.out.println(" 2 chislo");
+            b = in.nextInt();
+            a = a+b;
+            System.out.println(a);
         }
-
 
     }
 }

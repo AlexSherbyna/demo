945a6a3ae42143c1a214c62a6a4bbe57fde44bcc
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-10 22:39:59 +0200

    Palindrom

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -5,20 +5,36 @@ public class demo_2 {
 
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
-        int a, b, c ,d, v;
-        float a1;
+        int a, b, c ,d, v,t,g;
+
         a = in.nextInt();
-        if(a%2 == 0){
-            System.out.println(" 2 chislo drob");
-            a1 = in.nextFloat();
-            a1 = (a*a1);
-            System.out.println(a1);
+        b = a/10000;     //*----
+
+        c = (a/1000)%10; //-*---
+
+        d = (a/100)%10;  //--*--
+
+        v = (a/10)%10;   //---*-
+
+        t = a%10;        //----*
+
+            g = (t * 10000) + (v * 1000) + (d * 100) + (c * 10) + b;
+
+        if (b==0) g = (t * 1000) + (v * 100) + (d * 10) + c;
+        if (c==0) g = (t * 100) + (v * 10) + d;
+        if (d==0) g = (t * 10) + v;
+
+        if(a<99999){
+            if(a==g){
+                System.out.println("Palindrom");
+            } else {
+                System.out.println("Fake");
+            }
         } else {
-            System.out.println(" 2 chislo");
-            b = in.nextInt();
-            a = a+b;
-            System.out.println(a);
+            System.out.println("Big nuber");
         }
+
+
 
     }
 }

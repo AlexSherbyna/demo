290606d686699ad428c5207b2585db919e2eaef4
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-12 20:28:09 +0200

    Naoborot_2

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -6,38 +6,35 @@ public class demo_2 {
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
         int a = 0, b1, b2 ,b3, v,t,g;
+        int mass [] = new int[3];
         do{
-            a = in.nextInt();
-           if(a>999){
-               System.out.println("Fail_1");
-           } else {
-               b1 = a/100;
-                   if(b1 != 0){
-                       if(b1%2 ==0){
-                           b1+=1;
-                       } else {
-                           b1-=1;
-                       }
-                   }
-               b2 = (a/10)%10;
-                   if(b2 != 0){
-                       if(b2%2 ==0){
-                           b2+=1;
-                       } else {
-                           b2-=1;
-                       }
-                   }
-               b3 = a%10;
-                   if(b3 != 0){
-                       if(b3%2 ==0){
-                           b3+=1;
-                       } else {
-                           b3-=1;
-                       }
-                   }
+                        a = in.nextInt();
+                        if(a>999){
+                       System.out.println("Fail_1");
+                   } else {
 
-               System.out.println(b1+""+b2+""+b3);
-           }
+                           b1 = a/100;
+                           mass[0] = b1;
+
+                           b2 = (a/10)%10;
+                           mass[1] = b2;
+
+                           b3 = a%10;
+                           mass[2] = b3;
+
+                                for(int i = 0; i < mass.length; i++){
+                                   if(mass[i] != 0){
+                                       if(mass[i]%2 ==0){
+                                           mass[i]+=1;
+                                       } else {
+                                           mass[i]-=1;
+                                       }
+                                   }
+                               }
+
+
+                   }
+            System.out.println(mass[0]+""+mass[1]+""+mass[2]);
 
         }while (true);
 

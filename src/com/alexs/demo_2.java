e04de48ec30d1d5dd6d9f398bc61884fdd836cbb
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-12 19:24:53 +0200

    Naoborot

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -5,23 +5,42 @@ public class demo_2 {
 
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
-        int a = 0, b, c ,d, v,t,g;
+        int a = 0, b1, b2 ,b3, v,t,g;
+        do{
+            a = in.nextInt();
+           if(a>999){
+               System.out.println("Fail_1");
+           } else {
+               b1 = a/100;
+                   if(b1 != 0){
+                       if(b1%2 ==0){
+                           b1+=1;
+                       } else {
+                           b1-=1;
+                       }
+                   }
+               b2 = (a/10)%10;
+                   if(b2 != 0){
+                       if(b2%2 ==0){
+                           b2+=1;
+                       } else {
+                           b2-=1;
+                       }
+                   }
+               b3 = a%10;
+                   if(b3 != 0){
+                       if(b3%2 ==0){
+                           b3+=1;
+                       } else {
+                           b3-=1;
+                       }
+                   }
 
-        int nums [][] = new int[3][5];
+               System.out.println(b1+""+b2+""+b3);
+           }
 
-        for (int i = 0; i<3; i++){
-            for(int j = 0; j<5; j++){
-                nums[i][j] = (i+1)*(j+1);
-                System.out.println(i+" "+j+" "+nums[i][j]);
-            }
-        }
-        for(int x[]:nums){
-            for(int y:x){
-                System.out.println(y);
-                a +=y;
-            }
-        }
-        System.out.println(a);
+        }while (true);
+
 
     }
 }

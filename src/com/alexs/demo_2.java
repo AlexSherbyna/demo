1344a3d92e408e97031a2c9fb0aa74254d9df6b3
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-13 21:09:55 +0200

    &&

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -6,39 +6,16 @@ public class demo_2 {
     public static Scanner in=new Scanner(System.in);
     public static void main(String args[]) {
         int a = 0, b1, b2 ,b3, v,t,g;
+        double a1;
         int mass [] = new int[3];
         do{
-                        a = in.nextInt();
-                        if(a>999){
-                       System.out.println("Fail_1");
-                   } else {
-
-                           b1 = a/100;
-                           mass[0] = b1;
-
-                           b2 = (a/10)%10;
-                           mass[1] = b2;
-
-                           b3 = a%10;
-                           mass[2] = b3;
-
-                                for(int i = 0; i < mass.length; i++){
-                                   if(mass[i] != 0){
-                                       if(mass[i]%2 ==0){
-                                           mass[i]+=1;
-                                       } else {
-                                           mass[i]-=1;
-                                       }
-                                   }
-                               }
-
-
-                   }
-            System.out.println(mass[0]+""+mass[1]+""+mass[2]);
-
+            a = in.nextInt();
+           if(a>40 && a<100){
+               System.out.println("Yes");
+           } else {
+               System.out.println("Now");
+           }
         }while (true);
-
-
     }
 }
 

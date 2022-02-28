2c1a52a322751e9510f7027bea9f4277a7622fda
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-02-28 14:01:40 +0200

    Up-Down

--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -8,23 +8,19 @@ public class Demo_2 {
     public static Scanner in = new Scanner(System.in);
     public static void main(String args[]) {
 
-        int a,a1 = 7,a2 = 0,a3 = 0,a4 = 0, a5 = 0,a6 = 0, sum1,sum2;
-        int arr[] = { -3,0,2,4,5};
-        int aa = arr[arr.length-1];
-        int bb = arr[0];
-        while (bb<aa){
-            if((bb+aa)!= a1){
-                if((bb+aa)<a1){
-                    bb++;
-                } else {
-                    aa--;
-                }
-            } else {
-                System.out.println(" aa "+ aa +" bb "+ bb);
-                break;
+        int a,a1 = 0,a2 = 0,a3 = 0,a4 = 0, a5 = 0,a6 = 0, sum1,sum2;
+       while (true){
+           a = in.nextInt();
+            if(a < 13 && a!= 0 ){
+                if(a == 12 ) System.out.println("Zima");
+                if( a <= 2) System.out.println("Zima");
+                if(a<= 5 && a>= 3) System.out.println("Vesna");
+                if(a<= 8 && a>= 6) System.out.println("Leto");
+                if(a<= 11 && a>= 9) System.out.println("Osin");
             }
 
-        }
+
+       }
 
     }
 }

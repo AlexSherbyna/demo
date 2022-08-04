c41f47816017896ce36bb1abbef22cd4af750c08
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-08-04 22:02:36 +0300

    Random_2

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -10,9 +10,24 @@ public class Main {
         Random randomNumber = new Random();
            while (true) {
 
-              int result,minNumber,maxNumber;
-                maxNumber = -9 + (int) ((9-(-9)+1) * Math.random());
-               System.out.println(maxNumber);
+              int rand, randNumber_1, randNumber_2, randNumber_3;
+               System.out.println(" Number");
+
+               rand = (int) (Math.random() * 100);
+
+                     System.out.println(rand);
+                     System.out.println(" -- ");
+
+               randNumber_1 = (int)(Math.random() * rand);
+                    System.out.println(randNumber_1);
+
+               randNumber_2 =  (int)(Math.random() * rand);
+                     System.out.println(randNumber_2);
+
+               randNumber_3 =  (int)(Math.random() * rand);
+                    System.out.println(randNumber_3);
+
+
            }
 
         }

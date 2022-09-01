688736893da3e6d4e42f1af69f7e626a940952e9
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-09-01 21:36:48 +0300

    New_Game

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -9,45 +9,64 @@ public class Main {
     public static void main(String[] args) {
         MyClass_2 class_2 = new MyClass_2();
         String msg = null;
-        boolean move = true;
-        int num_1 , num_2, num_3, sum = 0;
-           while (move) {
+        int arr [][] = new int[11][3];
+        arr[0][0] = (0);
+        arr[0][1] = (1);
+        arr[0][2] = (2);
 
-                    System.out.println(" ----------");
-                    System.out.println(" Your numbers");
 
-                class_2.oneCard();
-                    num_1 = class_2.getA();
-                    System.out.println("First card " +num_1);
+        int player_1 = 0 , player_2 = 0 , num_3, num_4 , sum = 0;
+           while (class_2.getStoped()) {
 
-                class_2.oneCard();
-                    num_2 = class_2.getA();
-                    sum = num_1 + num_2;
-                    System.out.println("Second card " +num_2);
-                    System.out.println("Your sum :" +sum );
-                    System.out.println("Do you ned one card? Y/N");
-                    msg = in.nextLine();
+                for( int a = 1 ; a < 11 ;a++){
 
-                    if(msg.equals("Y")  || msg.equals("y")){
-                        class_2.oneCard();
-                        num_3 = class_2.getA();
-                        System.out.println("Third card " +num_3);
-                        sum+= num_3;
+                        System.out.println(" ----------");
+                        System.out.println("GAME " +a);
+                        System.out.println("Player 1 your number");
+
+                        arr[a][0] = a;
+                        class_2.cube();
+
+                        arr[a][1] = class_2.getA();
+                        player_1 += class_2.getA();
+
+                        System.out.println(class_2.getA());
+                        System.out.println("Player 2 your number");
+
+                        class_2.cube();
+                        arr[a][2] =  class_2.getA();
+                        player_2 += class_2.getA();
+
+                        System.out.println(class_2.getA());
+
+
+
+                        if(a == 10) {
+                            System.out.println("GAME OVER");
+                            class_2.sTop();
+                                if(player_1 != player_2) {
+                                    if (player_1 < player_2) {
+                                        System.out.println("Player 2 WIN!!");
+                                    } else {
+                                        System.out.println("Player 1 WIN!!");
+                                    }
+                                }
+
+                            for (int i = 0 ; i < 11 ; i++){
+                                for (int v = 0 ; v < 3 ; v++){
+
+                                    System.out.print(arr[i][v]);
+                                    System.out.print(" ");
+                                }
+                                System.out.println();
+                            }
+
+
+
+                        }
                     }
-
-               System.out.println(sum);
-               System.out.println(sum != 21 ? "Yo lose" : "Yo win");
-               System.out.println("One more? Y/N");
-
-               msg = in.nextLine();
-
-               if(msg.equals("N")  || msg.equals("n")){
-                   System.out.println(" GG my friend!");
-                   move = false;
-
-               }
-           }
-
-        }
-
+          }
     }
+
+}
+
--- src/com/alexs/MyClass_2.java
+++ src/com/alexs/MyClass_2.java
@@ -3,15 +3,15 @@ package com.alexs;
  public  class   MyClass_2 extends AbClass {
      private int a;
      private int b;
-     private boolean stoped = true;
+     private boolean run = true;
      private int maxVolume , minVolume , randomNumberDiapazon;
 
      public void setStoped(boolean stoped) {
-         this.stoped = stoped;
+         this.run = run;
      }
 
      public boolean getStoped() {
-         return stoped;
+         return run;
      }
 
      public int getA() {
@@ -68,7 +68,7 @@ package com.alexs;
          a = a1;
      }
      public void sTop () {
-         stoped = false;
+         run = false;
      }
      public void centerVolume () {
 
@@ -111,42 +111,19 @@ package com.alexs;
 
 
      }
-     public void oneCard () {
-         a = (int)(Math.random() * (10 - 1) + 1);
+     public void cube () {
+         a = (int)(Math.random() * (7 - 1) + 1);
 
      }
 
+     public static double multiply(double a, double b) {
+         return a * b;
+     }
 }
 
 
 
 
-  /*  void myClass_2_fun () {
 
-        if ((a1 + a2 + a3 != 180) || (a1 == 0) || (a2 == 0) || (a3 == 0)) System.out.println("Not triangle");
-         else   System.out.println("Triangle");
-    }
-    void show (String msg) {
-
-        System.out.println( msg +" "+(a1+a2+a3));
-    }
-    void how(int a) {
-        b4 = 1;
-        arr[0] = a/100;
-        arr[1] = (a/10)%10;
-        arr[2] = a%10;
-        if (arr[0] == arr[1]){
-            if (arr[1] != arr[2])  b4+=1;
-        } else  b4+=1;
-
-        if (arr[0] != arr[2]) b4+=1;
-        System.out.println("Число "+getA()+" содержит "+ b4+" разных  цифр");
-    }
-    void how( String msg) {
-        super.drob_3();
-        if(arr[0] != 0 &&(getA()%arr[0])== 0) System.out.println(msg + "Число делиться на " + arr[0]);
-        if(arr[1] != 0 && (getA()%arr[1])== 0) System.out.println(msg + "Число делиться на " + arr[1]);
-        if(arr[2] != 0 && (getA()%arr[2])== 0) System.out.println(msg + "Число делиться на " + arr[2]);
-    }*/
 
 

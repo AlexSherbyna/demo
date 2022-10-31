a6aad6f188061c3d4f9c29069b09e8970703ee4a
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-10-31 19:09:07 +0200

    Triengel

--- src/com/alexs/GameMagicNumber.java
+++ src/com/alexs/GameMagicNumber.java
@@ -1,99 +1,43 @@
 package com.alexs;
 
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
-import static java.lang.System.out;
-
 public class GameMagicNumber {
 
-    private Integer randomNumber, randomNumber_1, randomNumber_2 ;
-    private Integer maxNumber = 0;
-    private Integer minNumber = 0;
-    private Integer sameNumber = 0;
-    private Integer[] arr_1 = new Integer[2];
-    private Integer[] arr_2 = new Integer[2];
-    private Integer arr[] = new Integer[2];
-    private final String MAX_NUMBER = "Max number";
-    private final String MIN_NUMBER = "Min number";
-    private final String SAME_NUMBER = "Same number";
+    private Double randomNumber, randomNumber_1, randomNumber_2, randomNumber_3, randomNumber_4 ;
 
-   private Integer randomNumberGenerate() {
-       randomNumber = (int)(Math.random() * (77- 1) + 17);
+   private Double randomNumberGenerate() {
+       randomNumber = (Math.random() * 9.5) + 1;
+       randomNumber = Math.round(randomNumber * 10.0)/10.0;
        return randomNumber;
    }
 
    public void start() {
         randomNumber_1 = randomNumberGenerate();
         randomNumber_2 = randomNumberGenerate();
+        gipotenuza();
+        plosha();
 
-       chekNumber(randomNumber_1);
-       outText(randomNumber_1);
-       arr_1 = arr;
-       numberDown();
-
-       chekNumber(randomNumber_2);
-       outText(randomNumber_2);
-       arr_2 = arr;
-       arrSort(arr_1, arr_2);
-       bigOrNot(randomNumber_1, randomNumber_2);
-       outText(minNumber, maxNumber);
-       numberDown();
-
-   }
-
-   private void chekNumber(Integer number) {
-       randomNumber = number;
-       arr[0] = randomNumber /10;
-       arr[1] = randomNumber %10;
-       Arrays.sort(arr);
-       minNumber = arr[0];
-       maxNumber = arr[1];
-
-       if(arr[0] == arr[1])
-           sameNumber = arr[0];
-   }
-   private void arrSort(Integer arrOne[], Integer arrTwo[]) {
-
-       if (arrOne[0] > arrTwo[0]) {
-           minNumber = arrTwo[0];
-       } else {
-           minNumber = arrTwo[0];
-       }
-       if (arrOne[1] > arrTwo[1]) {
-           maxNumber = arrTwo[1];
-       } else {
-           maxNumber = arrTwo[1];
-       }
-   }
-
-   private void bigOrNot(Integer num_1, Integer num_2) {
-       if(num_1 > num_2){
-           out.println(num_1 +" "+"Big");
-       }else {
-           out.println(num_2 +" "+"Big");
-       }
-   }
-   private void numberDown() {
-       maxNumber = 0;
-       minNumber = 0;
-       sameNumber = 0;
-   }
-
-   private void outText(Integer outNumber ) {
-
-       out.println(outNumber+" "+"Random number");
-       out.println(maxNumber+" "+MAX_NUMBER);
-       out.println(minNumber+" "+MIN_NUMBER);
-       out.println(sameNumber+" "+SAME_NUMBER);
-
-   }
-   private void outText(Integer minNumber, Integer maxNumber ) {
-       out.println( "Small number"+ " " +minNumber);
-       out.println( "Max number"+ " " +maxNumber);
+        text(randomNumber_1,"A","Катет ");
+        text(randomNumber_2,"B","Катет ");
+        text(randomNumber_3, "C","Катет ");
+        text(randomNumber_4, "","Площа ");
 
 
    }
+
+   private void   text (Double number , String catet, String text) {
+       System.out.println(text +catet +" = " +number );
+   }
+   private void gipotenuza () {
+       randomNumber_3 = Math.sqrt (Math.pow(randomNumber_1,2) + Math.pow(randomNumber_2, 2)) ;
+       randomNumber_3 = Math.round(randomNumber_3 * 10.0)/10.0;
+   }
+   private void plosha () {
+       randomNumber_4 = 0.5 *(randomNumber_1 + randomNumber_2);
+       randomNumber_4 = Math.round(randomNumber_4 * 10.0)/10.0;
+   }
+
+
+
 
 }
 
--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -4,9 +4,25 @@ package com.alexs;
 
 public class Main {
 
+
     public static void main(String[] args) {
+        Runtime r = Runtime.getRuntime();
+        String s1 = new String("hi");
+        String s2 = new String("hi");
+        Integer number = 25000;
         GameMagicNumber game = new GameMagicNumber();
         game.start();
+
+
+//        System.out.println(System.nanoTime());
+//        ProcessBuilder p = new ProcessBuilder("calc");
+//        try {
+//            p.start();
+//        } catch (Exception e){
+//            System.out.println("Fail");
+//        }
+
+
     }
 }
 

221ec5fedce2186593e4d5aa6f9206a6f8c2df3c
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-11-04 13:56:57 +0200

    Cordinate

--- src/com/alexs/GameMagicNumber.java
+++ src/com/alexs/GameMagicNumber.java
@@ -2,49 +2,53 @@ package com.alexs;
 
 public class GameMagicNumber {
 
-    private Double randomNumber, randomNumber_1, randomNumber_2, randomNumber_3, randomNumber_4 ;
-    private Double max, min;
+    private Double randomNumber, randomNumber_1, randomNumber_2, gipotenuza, grad ;
 
    private Double randomNumberGenerate() {
-       randomNumber = (Math.random() * 899) + 100;
+       randomNumber = (Math.random() * 59) + -(30);
+       randomNumber = Math.ceil(randomNumber);
 
-       return Math.ceil(randomNumber);
+       return randomNumber;
    }
 
    public void start() {
         randomNumber_1 = randomNumberGenerate();
         randomNumber_2 = randomNumberGenerate();
-        randomNumber_3 = randomNumberGenerate();
-        maxMin(randomNumber_1, randomNumber_2, randomNumber_3);
 
-        text(randomNumber_1,"A","");
-        text(randomNumber_2,"B","");
-        text(randomNumber_3, "C","");
-        text(max,"Max", "");
-        text(min, "Min", "");
-        text((max - min),"Raznica", "");
-
-
-
+        text(randomNumber_1, "X", "");
+        text(randomNumber_2, "Y", "");
+        text(chetvert(randomNumber_1,randomNumber_2), ". Cetvert", "Blishe os " +toClose(randomNumber_1,randomNumber_2));
+        text(distans(randomNumber_1,randomNumber_2), "Distanse" , "");
+        text(grad(randomNumber_2, randomNumber_1), "Gradysu", "");
 
    }
-   private void maxMin (Double num1, Double num2, Double num3) {
+   private String toClose( Double x, Double y ) {
+       x = Math.abs(x);
+       y = Math.abs(y);
+       if(x < y) return "Y";
+       return "X";
+   }
 
-       max = Math.max(randomNumber_1, randomNumber_2);
-       max = Math.max(max, randomNumber_3);
-
-       min = Math.min(randomNumber_1, randomNumber_2);
-       min = Math.min(min, randomNumber_3);
+   private Double chetvert( Double x, Double y) {
+       if(y > 0 && x < 0) return 1.0;
+       if(y > 0 && x > 0) return 2.0;
+       if(y < 0 && x < 0) return 3.0;
+       if(y < 0 && x > 0) return 4.0;
+       return 0.0;
    }
 
    private void   text (Double number , String catet, String text) {
        System.out.println(text +catet +" = " +number );
-
    }
 
+   private Double distans ( Double x, Double y) {
+       return gipotenuza = Math.ceil(Math.hypot(x,y)) ;
+        }
 
-
-
+   private Double grad(Double x, Double y) {
+       grad = Math.atan( 1.0 *  y/ x);
+       return grad = Math.ceil(Math.toDegrees(grad)) ;
+   }
 
 }
 

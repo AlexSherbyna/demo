4a36fb1850a9c06fdc2257c1942b7702d604c9f5
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-12-10 15:13:28 +0200

    Lisr_Arr/Linked

--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -1,10 +1,38 @@
 package com.alexs;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.TreeSet;
+
 public class Main {
     public static void main(String[] args) {
 
-        new Fors();
+       // new Fors();
+        ArrayList<MailData> ar = new ArrayList<MailData>(50);
+        LinkedList<MailData> md;
 
+
+
+//        md.add(new MailData("Ola","Fing","ola@google.com","1259"));
+//        md.add(new MailData("Faa","Gint","Faa@google.com","3289"));
+
+        ar.add(new MailData("Ola","Fing","ola@google.com","1259"));
+        ar.add(new MailData("Faa","Gint","Faa@google.com","3289"));
+        ar.add(new MailData("rfraa","asdasdGint","asdadFaa@google.com","32289"));
+
+        md = new LinkedList<MailData>(ar);
+        System.out.println(md);
+        System.out.println(md.size());
+
+
+        System.out.println(ar);
+        System.out.println(ar.size());
+
+
+
+
+
+       
 //        System.out.println(System.nanoTime());
 //        ProcessBuilder p = new ProcessBuilder("calc");
 //        try {
--- src/com/alexs/demo_2.java
+++ src/com/alexs/demo_2.java
@@ -5,10 +5,7 @@ import java.util.Scanner;
 
 public class Demo_2 {
 
-    public static Scanner in = new Scanner(System.in);
-    public static void main(String args[]) {
-
 
     }
-}
+
 

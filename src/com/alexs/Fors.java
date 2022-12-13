ee9bc4f4da45a10cb135076543b9f5697213bba8
Alex_Sherbyna <alex.sherbyna1@gmail.com>
2022-12-13 14:39:40 +0200

    For_11.9

--- src/com/alexs/Fors.java
+++ src/com/alexs/Fors.java
@@ -3,39 +3,55 @@ package com.alexs;
 import java.util.Scanner;
 
 public class Fors {
+
+    Integer firstNumber,secondNumber, randomNumber;
     Scanner scanner = new Scanner(System.in);
+
     Fors(){
-        Integer a1,a2;
+
         System.out.println("first number");
-        a1 = scanner.nextInt();
+        firstNumber = scanner.nextInt();
 
         System.out.println("second number");
-        a2 = scanner.nextInt();
+        secondNumber = scanner.nextInt();
 
-        for (int i = 0; i < 20;i++) {
+        for (int i = 0; i < 15;i++) {
 
-            System.out.println(randomNumber(a1,a2));
+            System.out.print(randomNumber() +addSumvol(chekNegativNunmber(randomNumber)));
+            System.out.print(" ");
         }
-
     }
 
-    private void startFors() {
+    private Integer randomNumber() {
 
-    }
-    private Integer randomNumber(Integer firstNumber,Integer secondNumber) {
-        Integer randomNumber,helpNumber;
-        if (firstNumber > secondNumber) {
-            helpNumber = secondNumber;
-            secondNumber = firstNumber;
-            firstNumber = helpNumber;
-        }
+        chekAbsNumbers(firstNumber,secondNumber);
         randomNumber = firstNumber + (int) ((secondNumber - firstNumber + 1)*Math.random()) ;
         return randomNumber;
     }
-    private void generation() {
-        for (int i = 0; i < 20;i++) {
 
+    private void chekAbsNumbers(Integer firstNumber,Integer secondNumber) {
+
+        Integer helpNumber;
+
+        if (firstNumber > secondNumber) {
+            helpNumber = secondNumber;
+            this.secondNumber = firstNumber;
+            this.firstNumber = helpNumber;
         }
+    }
+
+    private boolean chekNegativNunmber(Integer isChekNumber) {
+        if(isChekNumber < 0) {
+           return true;
+        } else {
+          return   false;
+        }
+
+    }
+
+    private String addSumvol(Boolean isAddSumvol) {
+        if(isAddSumvol) return "?";
+        return "";
     }
 
 }
--- src/com/alexs/Main.java
+++ src/com/alexs/Main.java
@@ -7,26 +7,12 @@ import java.util.TreeSet;
 public class Main {
     public static void main(String[] args) {
 
-       // new Fors();
-        ArrayList<MailData> ar = new ArrayList<MailData>(50);
-        LinkedList<MailData> md;
+       new Fors();
 
 
 
-//        md.add(new MailData("Ola","Fing","ola@google.com","1259"));
-//        md.add(new MailData("Faa","Gint","Faa@google.com","3289"));
-
-        ar.add(new MailData("Ola","Fing","ola@google.com","1259"));
-        ar.add(new MailData("Faa","Gint","Faa@google.com","3289"));
-        ar.add(new MailData("rfraa","asdasdGint","asdadFaa@google.com","32289"));
-
-        md = new LinkedList<MailData>(ar);
-        System.out.println(md);
-        System.out.println(md.size());
 
 
-        System.out.println(ar);
-        System.out.println(ar.size());
 
 
 
